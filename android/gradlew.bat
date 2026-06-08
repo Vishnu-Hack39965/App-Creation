8362b60a464b9e2c43f845cb68226611372a5cfe
Vishnu-Hack <vishnu.406.39965@gmail.com>
2026-06-08 10:22:18 +0530

    Add gradlew.bat for Windows Gradle execution

---
+++ android/gradlew.bat
@@ -0,0 +1,18 @@
+@echo off
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+if defined JAVA_HOME goto findJavaFromJavaHome
+set JAVA_EXE=java.exe
+goto execute
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+:execute
+set CLASSPATH=%DIRNAME%\gradle\wrapper\gradle-wrapper.jar
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+exit /b %ERRORLEVEL%
